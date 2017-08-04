7b4e06f76a8a9efca62cd604dc8f1fc827c18fc3
theinloft <theinloft@gmail.com>
2017-08-04 16:08:16 -0300

    commit inicial
    adicionando os arquivos

---
+++ images/index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+   <meta charset="Content-Type: text/html; charset=UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="utf-8">
+    <meta name="keywords" content="Hospedagem e desenvolvimento de sites, Loja Virtual">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Cabana da Web</title>
+
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+        <body>
+
+ <div class="topo"><br>
+              </div>
+                  <div class="container-fluid" align="center" >
+
+                  <img src="img/Logo.png" class="img-responsive img-rounded" alt="logo">
+                  <h1>Em Construção, em breve, novidades para vocês. </h1>
+
+                  </div>
+
+        
+
+          <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+          <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+          <!-- Include all compiled plugins (below), or include individual files as needed -->
+          <script src="js/bootstrap.min.js"></script>
+         
+      </body>
+</html> 
---
+++ index.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Cabana da Web</title>
+	<link rel="stylesheet" type="text/css" href="Libraries/estilos.css">
+
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+</head>
+<body data-spy="scroll" data-target=".menu" data-offset="12">
+		<header>
+		<div class="menu">
+			<img class="img" src="images/Logo.png">
+				<ul>
+					<li><a href="#home">Home</a></li>
+					<li><a href="#sobrenos">Sobre Nós</a></li>
+					<li><a href="#">Serviços</a></li>
+					<li><a href="#">Contato</a></li>
+				</ul>
+		</div>
+		</header>
+
+
+		<main>
+		<div class="parallax"></div>
+
+		<div class="sobreposicao1" id="home">
+	<p>	Fornecemos Hospedagem e manunteção de sites e Lojas Virtuais, serviços de desenvolvimento para web(Wordpress,Joomla e Magento),
+você tem uma ideía, podemos ajudar.</p>
+		</div>
+			
+		<div class="parallax2"></div>	
+		</main>
+
+
+
+		<div class="sobreposicao2" id="sobrenos">
+			<p>Somos uma empresa jovem no mercado, iniciamos nossas atividades neste semestre de 2017, buscando sempre um bom atendimento e bom relacionamento com nossos clientes.</p>
+
+			<p>Se você tem uma ideia, converse conosco, podemos juntos alcançar seus objetivos. </p>	
+
+		<br>
+
+		</div>
+
+		<div class="sobreposicao3">
+
+			<img src="images/Joomla.png" width="400px">
+			<img src="images/Magento.png" width="400px">
+			<img src="images/Wordpress.png" width="400px">
+
+
+		</div>
+
+
+		<footer>
+		<div class="bordasuperior-footer"> <br></div>
+		<br>
+
+			
+		</footer>
+			<div class="bordasuperior-footer"> <br></div>
+		
+
+</body>
+</html>
